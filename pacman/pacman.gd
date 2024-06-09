ef88a3bc09bfdb12cc6e25ddb659a035dfdb8b87
edudf <edudf90@gmail.com>
2024-06-09 17:26:08 -0300

    Fix problems with pacman's movement.

--- pacman/pacman.gd
+++ pacman/pacman.gd
@@ -5,41 +5,58 @@ extends Node2D
 const SPEED = 60.
 const SPRITE_RADIUS = 8.
 
-var direction = Vector2i(0, 0)
+var direction = Vector2i.ZERO
 var current_position : Vector2i
+var is_moving = false
 
 func _ready():
 	current_position = tilemap.local_to_map(position)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") && (
+	(!is_moving && is_target_position_walkable(current_position + Vector2i.UP)) || \
+	(is_moving && direction == Vector2i.DOWN)):
 		direction = Vector2i.UP
 		$AnimatedSprite2D.rotation_degrees = -90
 		$AnimatedSprite2D.play("moving")
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") && (
+	(!is_moving && is_target_position_walkable(current_position + Vector2i.DOWN)) || \
+	(is_moving && direction == Vector2i.UP)):
 		direction = Vector2i.DOWN
 		$AnimatedSprite2D.rotation_degrees = 90
 		$AnimatedSprite2D.play("moving")
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") && (
+	(!is_moving && is_target_position_walkable(current_position + Vector2i.RIGHT)) || \
+	(is_moving && direction == Vector2i.LEFT)):
 		direction = Vector2i.RIGHT
 		$AnimatedSprite2D.rotation_degrees = 0
 		$AnimatedSprite2D.play("moving")
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") && (
+	(!is_moving && is_target_position_walkable(current_position + Vector2i.LEFT)) || \
+	(is_moving && direction == Vector2i.RIGHT)):
 		direction = Vector2i.LEFT
 		$AnimatedSprite2D.rotation_degrees = 180
 		$AnimatedSprite2D.play("moving")
-	move(delta)
+	if direction != Vector2i.ZERO:
+		move(delta)
 
 func move(delta):
 	var target_position = current_position + direction
+	is_moving = true
 	if tilemap.local_to_map(position - direction * SPRITE_RADIUS) == target_position:
+		is_moving = false
 		current_position = target_position
 		target_position = current_position + direction
 	var local_target_position = tilemap.map_to_local(target_position)
-	var tile_data = tilemap.get_cell_tile_data(0, target_position)
-	if tile_data && tile_data.get_custom_data("walkable"):
+	if is_target_position_walkable(target_position):
 		var local_target = local_target_position + direction * SPRITE_RADIUS
 		position.x = move_toward(position.x, local_target.x, delta * SPEED)
 		position.y = move_toward(position.y, local_target.y, delta * SPEED)
 	else:
+		is_moving = false
+		direction = Vector2i.ZERO
 		$AnimatedSprite2D.stop()
+
+func is_target_position_walkable(target_position):
+	var tile_data = tilemap.get_cell_tile_data(0, target_position)
+	return tile_data && tile_data.get_custom_data("walkable")
